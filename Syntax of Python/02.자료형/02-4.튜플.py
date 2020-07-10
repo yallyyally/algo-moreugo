a4c3e0fd191bb697340ffd9a2482595bc42c7667
yallyyally <jhshin0028@gmail.com>
2020-07-10 22:28:29 +0900

    Update 02-4.튜플.py
    튜플 자료형 연산하기 - 인덱싱, 슬라이싱, 더하기, 곱하기 ,길이 구하기

--- Syntax of Python/02.자료형/02-4.튜플.py
+++ Syntax of Python/02.자료형/02-4.튜플.py
@@ -9,11 +9,16 @@ t4=(1,5,'gh0') #다른 자료형도 ㅇㅋ
 t5=('ㅁ','b',(5,'f')) #튜플 속의 튜플
 
 #del t1[0] //삭제 불가-TypeError: 'tuple' object doesn't support item deletion
-#t1[0]='c' //추가 불가-TypeError: 'tuple' object does not support item assignment
-#t3[1]='v' //수정 불가-TypeError: 'tuple' object does not support item assignment
+#t1[0]='c' //수정 불가-TypeError: 'tuple' object does not support item assignment
+#t3[1]='v' //추가 불가-TypeError: 'tuple' object does not support item assignment
 
 #1. 인덱싱하기
+print(t4[2]) #'gh0
 #2. 슬라이싱하기
+print(t4[:-2])#(1,)
 #3. 더하기
+print(t3+t4) #(1,2,3,1,5,'gh0')
 #4. 곱하기
+print(t3*2) #(1,2,3,1,2,3)
 #5. 길이 구하기
+print(len(t5)) #3
