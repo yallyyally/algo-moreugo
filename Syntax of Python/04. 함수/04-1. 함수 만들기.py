677c6a82db73c060392ae288025cc318783c1508
yallyyally <jhshin0028@gmail.com>
2020-07-27 15:59:39 +0900

    Create 04-1. 함수 만들기.py

---
+++ Syntax of Python/04. 함수/04-1. 함수 만들기.py
@@ -0,0 +1,33 @@
+def add(a,b):
+    c=a+b
+    #print(c)
+    return c
+
+def add2(a,b):
+    c=a+b
+    d=a-b
+    return c,d #tuple 형태로 리턴.
+
+#소수면 true, 소수가 아니면 false
+def isPrime(x):
+    for i in range(2,x): #2 ~ x-1
+        if x%i==0:
+            return False
+    return True
+
+#함수는 항상 mainscript 위에 있어야.....define 되있단거 알아야 됨
+
+# add(3,2)
+print(add(3,2)) #5
+print(add2(4,8)) #(12,-4)
+
+print(isPrime(13)) #True
+print(isPrime(14)) #False
+
+a = [12,13,7,9,19]
+for y in a:
+    if isPrime(y):
+        #소수면 출력
+        print(y,end=' ')
+#13 7 19
+
