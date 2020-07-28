dab9a317ee0c6c0e23ad6d3d0ff97bdb0f0f1206
yallyyally <jhshin0028@gmail.com>
2020-07-28 22:50:18 +0900

    Create k번째 수.py

---
+++ Section2/k번째 수.py
@@ -0,0 +1,26 @@
+#k번째 수 찾기
+#N개의 숫자로 이루어진 숫자열.
+#해당 문자열 중 s~e를 오름차순 정렬했을 때 k번째로 나타는 숫자 출력
+
+def KthNum():
+    N,s,e,k=map(int,input().split())
+    #배열 입력 받기
+    numbers=list(map(int,input().split()))
+    #s~e만 소팅
+    numbers_filtered = numbers[s-1:e]
+    numbers_filtered.sort()
+    return numbers_filtered[k-1]
+
+
+#테스트케이스 수
+T = int(input())
+# answers = [range(T)] -> range형 오브젝트 하나인 배열
+# 한번에 테스트 케이스 별 답 출력하기 위함.
+answers = [0 for i in range(T)]
+
+for i in range(T):
+    answers[i]=(KthNum())
+
+for i in range(T):
+    print("#",i+1,' ',answers[i])
+
