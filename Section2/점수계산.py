abff4ea7cd5bf22e1ce96254cfd2aace8d53a025
yallyyally <jhshin0028@gmail.com>
2020-08-03 19:02:31 +0900

    점수 계산

---
+++ Section2/점수계산.py
@@ -0,0 +1,21 @@
+#점수 계산
+#OX퀴즈에서 연속적으로 맞힐 경우 가산점.
+#+1....+K, 틀리면 0점, 맞으면 또 +..
+#문제수(N)과 채점 결과 제공 (맞으면 1,틀리면 0)
+
+N = int(input())
+results=list(map(int,(input().split())))
+
+score=0
+series=0 #연속 몇갠지
+for i in range(N):
+    #맞음
+    if results[i]:
+        series +=1
+        score += series
+
+    #틀리면 연속 사라짐..
+    else:
+        series=0
+
+print(score)
