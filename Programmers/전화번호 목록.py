14a384ff9f28d0ad664882202bf3a71bc548bb96
yallyyally <jhshin0028@gmail.com>
2020-09-04 14:14:18 +0900

    전화번호 목록 해결
    접'두'어의 의미를 잘못 알고 있었다...ㅎ

--- Programmers/전화번호 목록.py
+++ Programmers/전화번호 목록.py
@@ -3,12 +3,12 @@ def solution(phone_book):
 
     for x in range(len(phone_book)):
         for y in range(x + 1, len(phone_book)):
-            if len(phone_book[x]) <= len(phone_book[y]):
-                if phone_book[y].find(phone_book[x]) != -1:
+            if len(phone_book[x]) < len(phone_book[y]):
+                if phone_book[y].find(phone_book[x]) == 0:
                     answer = False
                     break
             else:
-                if phone_book[x].find(phone_book[y]) != -1:
+                if phone_book[x].find(phone_book[y]) == 0:
                     answer = False
                     break
         if answer == False:
