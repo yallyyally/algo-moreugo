cd8165db2ca0fc4487be56279456cb7ccfd5a1d8
yallyyally <jhshin0028@gmail.com>
2020-09-05 18:28:01 +0900

    [2798] 블랙잭
    7개월만에 해결ㅋㅋㅋㅋ

--- Baekjoon/[2798] 블랙잭.py
+++ Baekjoon/[2798] 블랙잭.py
@@ -1,6 +1,25 @@
 N,M = map(int,input().split())
-cards = []
-for _ in range(N):
-    cards.append(int(input()))
+cards = list(map(int,input().split()))
 
-#M
+cards.sort(reverse=True)
+sum = 0
+max = 0
+for i in range(N):
+    sum = cards[i]
+    if sum>=M:
+        continue
+    for j in range(i+1,N):
+        sum+=cards[j]
+        #미리 처리
+        if sum>=M:
+            sum-=cards[j]
+            continue
+        for k in range(j+1,N):
+            if sum+cards[k]<=M and max<sum+cards[k]:
+                max = sum+cards[k]
+        else:
+            sum-=cards[j]
+    else:
+        sum-=cards[i]
+
+print(max)
