a00f47bf575a6d8772771c03679b8113aade57c3
yallyyally <jhshin0028@gmail.com>
2020-09-18 19:32:30 +0900

    격자판 최대합

--- Section3. 탐색&시뮬레이션/격자판 최대합.py
+++ Section3. 탐색&시뮬레이션/격자판 최대합.py
@@ -1,10 +1,40 @@
 import sys
-#행, 열, 대각선 ->열 -> 열,대각선
-#행, 행,행, 행
+
+# 행, 열, 대각선 ->열 -> 열,대각선
+# 행, 행,행, 행
 
 N = int(sys.stdin.readline())
 board = []
 for i in range(N):
-    board.append([])
-print(board)
-#for i in range()
+    board.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+# 격자판 입력 완료
+max = 0
+sums = [0]*3
+
+for i in range(N):
+    #0. 각 행의 합
+    if max<sum(board[i]):
+        max = sum(board[i])
+    if i==0:
+        sum_l=0 #대각선(왼)
+        sum_s=0 #대각선(오)
+        for j in range(N):
+            #1. 대각선 합
+            sum_l +=board[j][j]
+            #2. 대각선합 2
+            sum_s+=board[j][N-1-j]
+
+        sums[0],sums[1] = sum_l,sum_s
+        sums.sort(reverse=True)
+
+        if max < sums[0]:
+            max= sums[0]
+    #3.열의 합
+    sums = 0
+    for j in range(N):
+        sums+=board[j][i]
+    if max<sums:
+        max = sums
+
+print(max)
