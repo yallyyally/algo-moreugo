e6035392d5afd51daeb71303c03d1f9757422247
yallyyally <jhshin0028@gmail.com>
2021-07-11 16:37:09 +0900

    [프로그래머스] 뉴스 클러스터링

--- Baekjoon/[2156] 포도주 시식.java
+++ Baekjoon/[2156] 포도주 시식.java
@@ -22,7 +22,7 @@ class Main{
 		for(int i=1; i<N; i++) {
 			//이전것도 같이 선택할 경우 -> 두번째 전 건 무조건 선택 안함
 			dp[i] = i >=3 ? arr[i] + arr[i-1] + dp[i-3] : arr[i] + arr[i-1];
-			//이전건 선택 x -> 두번째 꺼 관심 x
+			//이전건 선택 x -> 두번째 전 꺼 관심 x
 			dp[i] = i != 1 ? (dp[i] > dp[i-2] + arr[i] ? dp [i] : dp[i-2] + arr[i]):(dp[i] > arr[i] ? dp[i] : arr[i]);
 			//현재 원소 선택하지 않을 경우
 			dp[i] = dp[i-1] < dp[i] ? dp[i] : dp[i-1];
